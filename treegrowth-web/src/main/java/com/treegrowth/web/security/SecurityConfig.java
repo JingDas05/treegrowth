d34f8bc9b1b32c3ee0e9b3a955903257f763b2fb
wusi <wusi@hooying.com>
2016-10-10 14:23:20 +0800

    add in memory user security

---
+++ treegrowth-web/src/main/java/com/treegrowth/web/security/SecurityConfig.java
@@ -0,0 +1,81 @@
+package com.treegrowth.web.security;
+
+import com.treegrowth.model.security.Authority;
+import com.treegrowth.web.security.userdetails.TgUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import static com.treegrowth.model.security.Authority.USER;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+    @Autowired
+    private TgUserDetailsService tgUserDetailsService;
+
+//    @Autowired
+//    public void configGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.userDetailsService(tgUserDetailsService);
+//    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+            .inMemoryAuthentication()
+                .withUser("wusi").password("123123").roles("USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                    .antMatchers("/css/**", "/index").permitAll()
+                    .antMatchers("/user/**").hasRole("USER")
+                    .and()
+                .formLogin()
+                    .loginPage("/login")
+                    .failureUrl("/login-error")
+                    .and()
+                .csrf()
+                    .disable();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//            .authorizeRequests()
+//                .antMatchers(
+//                    "/api/**"
+//                )
+//                .permitAll()
+//                .anyRequest().hasRole(USER.name())
+//                .and()
+//            .formLogin()
+//                .loginProcessingUrl("/login")
+//                .loginPage("/login")
+//                .failureUrl("/login?error")
+//                .usernameParameter("username")
+//                .passwordParameter("password")
+//                .and()
+//            .logout()
+//                .permitAll()
+//                .and()
+//            .csrf()
+//                .disable();
+//    }
+
+}
--- treegrowth-web/src/main/java/com/treegrowth/web/security/SecurityWebApplicationInitializer.java
+++
@@ -1,6 +0,0 @@
-package com.treegrowth.web.security;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-}
