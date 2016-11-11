e438469b9524fa875d84c738f042c86710386f3f
wusi <wusi@hooying.com>
2016-11-11 12:38:35 +0800

    add spring integration maven dependency

--- treegrowth-message-queue/src/main/java/com/treegrowth/message/quene/cofiguration/Config.java
+++ treegrowth-message-queue/src/main/java/com/treegrowth/message/quene/cofiguration/Config.java
@@ -3,12 +3,15 @@ package com.treegrowth.message.quene.cofiguration;
 import com.treegrowth.message.quene.core.CommonMessage;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.IntegerDeserializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.KafkaListenerContainerFactory;
 import org.springframework.kafka.core.*;
 import org.springframework.kafka.support.converter.StringJsonMessageConverter;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
@@ -21,28 +24,49 @@ import java.util.Map;
 @EnableKafka
 public class Config {
 
+    @Value("${kafka.topic}")
+    private String topic;
+
+    @Value("${kafka.messageKey}")
+    private String messageKey;
+
+    @Value("${kafka.broker.address}")
+    private String brokerAddress;
+
+    @Value("${kafka.zookeeper.connect}")
+    private String zookeeperConnect;
+
     @Bean
-    ConcurrentKafkaListenerContainerFactory<String, CommonMessage> kafkaListenerContainerFactory() {
-        ConcurrentKafkaListenerContainerFactory<String, CommonMessage> factory = new ConcurrentKafkaListenerContainerFactory<>();
+    public KafkaListenerContainerFactory<?> kafkaJsonListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<Integer, String> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory());
-        factory.setConcurrency(3);
-        factory.getContainerProperties().setPollTimeout(3000);
         factory.setMessageConverter(new StringJsonMessageConverter());
         return factory;
     }
 
+//    @Bean
+//    ConcurrentKafkaListenerContainerFactory<String, CommonMessage> kafkaListenerContainerFactory() {
+//        ConcurrentKafkaListenerContainerFactory<String, CommonMessage> factory = new ConcurrentKafkaListenerContainerFactory<>();
+//        factory.setConsumerFactory(consumerFactory());
+//        factory.setConcurrency(3);
+//        factory.getContainerProperties().setPollTimeout(3000);
+//        factory.setMessageConverter(new StringJsonMessageConverter());
+//        return factory;
+//    }
+
     @Bean
-    public KafkaTemplate<String, CommonMessage> kafkaTemplate() {
+    public KafkaTemplate<Integer, String> kafkaTemplate() {
         return new KafkaTemplate<>(producerFactory());
     }
 
     @Bean
-    public ProducerFactory<String, CommonMessage> producerFactory() {
+    public ProducerFactory<Integer, String> producerFactory() {
         return new DefaultKafkaProducerFactory<>(producerConfigs());
     }
 
     @Bean
-    public ConsumerFactory<String, CommonMessage> consumerFactory() {
+    public ConsumerFactory<Integer, String> consumerFactory() {
         return new DefaultKafkaConsumerFactory<>(consumerConfigs());
     }
 
@@ -54,8 +78,8 @@ public class Config {
         props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
         props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "100");
         props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "15000");
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, IntegerDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         return props;
     }
 
@@ -67,11 +91,13 @@ public class Config {
         props.put(ProducerConfig.BATCH_SIZE_CONFIG, 16384);
         props.put(ProducerConfig.LINGER_MS_CONFIG, 1);
         props.put(ProducerConfig.BUFFER_MEMORY_CONFIG, 33554432);
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, IntegerDeserializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         // value to block, after which it will throw a TimeoutException
         props.put(ProducerConfig.MAX_BLOCK_MS_CONFIG, 5000);
         return props;
     }
+
+
 
 }
